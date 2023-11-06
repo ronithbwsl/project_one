7786a6f0cd36edf8033e8d1e4f15a8421afc2a4a
Ronit Kumar Gupta <ronit.gupta@hbwsl.com>
2023-11-06 18:22:55 +0530

    Fixed Mistakes and generated whitelist file

--- app/code/ronit-vendor/ronit-module/registration.php
+++ app/code/ronit-vendor/ronit-module/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'RonitVendor_RonitModule', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ronit-vendor_ronit-module', __DIR__);
